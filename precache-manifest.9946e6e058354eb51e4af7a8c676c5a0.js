553e0e60b7d01efa84abb15a0e1b70747eec9620
HemantYadav <hy27.1999@gmail.com>
2019-07-03 13:33:12 +0530

    Updates

---
+++ precache-manifest.9946e6e058354eb51e4af7a8c676c5a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892d538ba26f27600749ce7c6a875f55",
+    "url": "/chatter/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/chatter/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2e127f2687691c5f803d",
+    "url": "/chatter/static/css/main.39dbcb6b.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/chatter/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "2e127f2687691c5f803d",
+    "url": "/chatter/static/js/main.0c00e1cf.chunk.js"
+  },
+  {
+    "revision": "4b835c2d5ad331911f11",
+    "url": "/chatter/static/js/runtime~main.98ac0f0d.js"
+  },
+  {
+    "revision": "85a3573f5c98205e90bef589891940d2",
+    "url": "/chatter/static/media/ba1.85a3573f.jpg"
+  }
+]);
